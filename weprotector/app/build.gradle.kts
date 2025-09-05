029aec9c2d4df870ba7925bb693dd0d4b351be83
VyankteshKoli <vyankteshkoli8804@gmail.com>
2025-09-05 22:03:03 +0530

    pushh

--- weprotector/app/build.gradle.kts
+++ weprotector/app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
+
 
 android {
     namespace = "com.example.weprotector"
@@ -13,16 +15,19 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -30,26 +35,28 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // Firebase
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation(libs.firebase.firestore)
-    implementation(libs.firebase.storage)
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("com.google.firebase:firebase-crashlytics:19.0.0")
+
+    // UI
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation("com.github.bumptech.glide:glide:4.15.1")
     implementation(libs.firebase.crashlytics.buildtools)
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
+    // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    implementation("com.squareup.picasso:picasso:2.71828")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-
-
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
-
 }
--- weprotector/build.gradle.kts
+++ weprotector/build.gradle.kts
@@ -1,11 +1,11 @@
-
-
 buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
+    repositories {
+        google()
+        mavenCentral()
     }
-}
-
-plugins {
-    id("com.android.application") version "8.4.1" apply false
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.1")
+        classpath("com.google.gms:google-services:4.4.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9") // ✅ add this
+    }
 }
